f652019d6782d958e0e1d4d6414db02cc1c8cad3
kaksity <39730877+kaksity@users.noreply.github.com>
2021-10-15 11:56:53 +0100

    Worked on the Add Account Feature

---
+++ src/CashBook.UI/Utilities/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashBook.UI.Utilities
+{
+    public static class Utility
+    {
+        public static bool IsNumeric(string input)
+        {
+            int outInt = 0;
+            long outLong = 0;
+            decimal outDecimal = 0;
+            if (int.TryParse(input,out outInt) == true || long.TryParse(input,out outLong) == true || decimal.TryParse(input, out outDecimal) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static decimal ParseNumber(string input)
+        {
+            return decimal.Parse(input);
+        }
+    }
+}
