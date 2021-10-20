c88a718be8623d41fc00ec63408eb77af81a8b4b
kaksity <39730877+kaksity@users.noreply.github.com>
2021-10-20 12:05:28 +0100

    Added the Create and Read Transaction Descriptions Dtos

---
+++ src/CashBook.Dtos/TransactionDescription/CreateTransactionDescriptionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashBook.Dtos.TransactionDescription
+{
+    class CreateTransactionDescriptionDto
+    {
+        public string DescriptionName { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/CashBook.Dtos/TransactionDescription/ReadTransactionDescriptionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashBook.Dtos.TransactionDescription
+{
+    class ReadTransactionDescriptionDto
+    {
+        public string TransactionDescriptionId { get; set; }
+        public string DescriptionName { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+    }
+}
