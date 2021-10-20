4915530f24fab7a14544a7a3e5bbb4dc67af52e5
kaksity <39730877+kaksity@users.noreply.github.com>
2021-10-20 11:44:05 +0100

    Added a Model

---
+++ src/CashBook.Models/TransactionDescription/TransactionDescriptionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashBook.Models.TransactionDescription
+{
+    public class TransactionDescriptionModel
+    {
+        public string TransactionDescriptionId { get; set; }
+        public string DescriptionName { get; set; }
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+        public string IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
